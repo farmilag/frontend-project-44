88c3868036145b16a97a7f442c636fe41f9d9859
farmilag <roman.kudrya007@gmail.com>
2025-02-06 14:45:14 +0300

    Дороботка

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -17,7 +17,7 @@ function isPrime(number) {
 
 function playGame() {
     const name = greetUser();
-    console.log("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+    console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
     for (let i = 0; i < 3; i++) {
         const number = Math.floor(Math.random() * 100) + 1; // Генерация числа от 1 до 100
