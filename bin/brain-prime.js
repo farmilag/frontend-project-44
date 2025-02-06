6650262488897120371c5b6d8b7eef8d8080f097
farmilag <roman.kudrya007@gmail.com>
2025-02-06 14:36:10 +0300

    Дороботка

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 import { greetUser } from '../src/cli.js';
 import readlineSync from 'readline-sync';
 
@@ -7,38 +8,34 @@ function getUserAnswer() {
 }
 
 function isPrime(number) {
-    for (let i = 2; i < number; i++) {
-      if (number % i === 0) return false;
+    if (number < 2) return false; // Числа меньше 2 не являются простыми
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) return false;
     }
-    return number !== 1;
-  }
+    return true;
+}
 
 function playGame() {
     const name = greetUser();
-    console.log("Answer 'yes' if given number is prime. Otherwise answer 'no'.")
+    console.log("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+
     for (let i = 0; i < 3; i++) {
-        const number = Math.floor(Math.random() * 15) + 1
+        const number = Math.floor(Math.random() * 100) + 1; // Генерация числа от 1 до 100
+        console.log(`Question: ${number}`); // Правильный формат вывода вопроса
 
-        console.log(number)
         const userAnswer = getUserAnswer();
-        let corectAnswer
-        if (isPrime(number) == true) {
-            corectAnswer = "yes"
-        }
-        else {
-            corectAnswer = "no"
-        }
+        const corectAnswer = isPrime(number) ? "yes" : "no";
 
-        if (userAnswer == corectAnswer) {
+        if (userAnswer === corectAnswer) {
             console.log("Correct!");
-        }
-        else {
-            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${corectAnswer}'.`);  
-            console.log(`Let's try again, ${name}`);
+        } else {
+            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${corectAnswer}'.`);
+            console.log(`Let's try again, ${name}!`);
             return;
         }
     }
+
     console.log(`Congratulations, ${name}!`);
 }
-playGame();
 
+playGame();
