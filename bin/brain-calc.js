3a7622751e4509e43abb5e6ae6317fa9a6b6cee9
farmilag <roman.kudrya007@gmail.com>
2025-02-07 14:48:59 +0300

    Дороботка

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -4,36 +4,37 @@ import greetUser from '../src/cli.js';
 import { getUserAnswer, generationRandomNumer, getResponseProcessing } from '../src/commonFun.js';
 
 const name = greetUser();
-console.log('Find the greatest common divisor of given numbers.');
-
-let correctAnswers = 0;
-
-function gcd(x, y) {
-  let a = x;
-  let b = y;
-  while (b !== 0) {
-    const temp = b;
-    b = a % b;
-    a = temp;
-  }
-  return a;
-}
+console.log('What is the result of the expression?');
 
 for (let i = 0; i < 3; i += 1) {
-  const number1 = generationRandomNumer(99, 1);
-  const number2 = generationRandomNumer(99, 1);
-  console.log(`Question: ${number1} ${number2}`);
+  const number1 = generationRandomNumer(100, 1);
+  const number2 = generationRandomNumer(100, 1);
+  const operators = ['*', '+', '-'];
+  const randomIndex = Math.floor(Math.random() * operators.length);
+  const randomOperator = operators[randomIndex];
+  console.log(`Question: ${number1} ${randomOperator} ${number2}`);
+
+  let result;
+  switch (randomOperator) {
+    case '+':
+      result = number1 + number2;
+      break;
+    case '-':
+      result = number1 - number2;
+      break;
+    case '*':
+      result = number1 * number2;
+      break;
+    default:
+      result = null;
+  }
 
   const userAnswer = getUserAnswer();
-  const correctAnswer = gcd(number1, number2);
 
-  if (getResponseProcessing(userAnswer, correctAnswer, name)) {
-    correctAnswers += 1;
-
-    if (correctAnswers === 3) {
-      console.log(`Congratulations, ${name}!`);
-    }
-  } else {
+  if (!getResponseProcessing(userAnswer, result, name)) {
     break;
+  }
+  if (i === 2) {
+    console.log(`Congratulations, ${name}!`);
   }
 }
